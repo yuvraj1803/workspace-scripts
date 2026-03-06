c4cdfaa091e3d5254bf1ce67121bd20447ccfc96
yuvraj1803 <ysakshith@gmail.com>
2026-03-06 18:18:22 +0530

    run: modify run scripts with rootfs change

--- run/run-edk2.sh
+++ run/run-edk2.sh
@@ -1,13 +1,13 @@
 qemu-system-aarch64 \
 -M virt \
 -cpu cortex-a72 \
--m 4G \
+-m 128M \
 -smp 2  \
---drive file=buildroot/output/images/rootfs.ext2,if=none,format=raw,id=hd0 \
+--drive file=rootfs/noble-base-arm64.img,if=none,format=raw,id=hd0 \
 --device virtio-blk-device,drive=hd0 \
 --kernel linux/arch/arm64/boot/Image \
 -bios edk2/Build/ArmVirtQemuKernel-AARCH64/RELEASE_GCC5/FV/QEMU_EFI.fd \
 --nographic \
 --serial mon:stdio \
---append "rootwait nokaslr root=/dev/vda rw init=/sbin/init console=ttyAMA0"
+--append "rootwait nokaslr root=/dev/vda rw init=/bin/bash console=ttyAMA0"
 
--- run/run-linux.sh
+++ run/run-linux.sh
@@ -1,12 +1,12 @@
 qemu-system-aarch64 \
 -M virt,virtualization=on \
 -cpu cortex-a72 \
--m 4G \
+-m 128M \
 -smp 2  \
---drive file=buildroot/output/images/rootfs.ext2,if=none,format=raw,id=hd0 \
+--drive file=rootfs/noble-base-arm64.img,if=none,format=raw,id=hd0 \
 --device virtio-blk-device,drive=hd0 \
 --kernel linux/arch/arm64/boot/Image \
 --nographic \
 --serial mon:stdio \
---append "rootwait nokaslr root=/dev/vda rw init=/sbin/init console=ttyAMA0"
+--append "rootwait nokaslr root=/dev/vda rw init=/bin/bash console=ttyAMA0"
 
